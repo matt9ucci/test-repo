cc67f5e3f88d20513af07cc4a024cbe3b6a5ff2e
matt9ucci <matt9ucci@gmail.com>
2016-07-17 02:27:09 +0900

    github code search

--- Firefox/user.js
+++ Firefox/user.js
@@ -2,3 +2,8 @@ user_pref("browser.tabs.warnOnClose", false);
 user_pref("browser.tabs.warnOnCloseOtherTabs", false);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
 user_pref("general.warnOnAboutConfig", false);
+
+user_pref("places.history.expiration.max_pages", 10000);
+
+user_pref("extensions.selectedsearch.autocptext", true);
+
