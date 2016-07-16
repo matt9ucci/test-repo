2acfe42d617febb7eba923f6666b351c52439a5a
matt9ucci <matt9ucci@gmail.com>
2016-07-16 18:26:03 +0900

    firefox pref

---
+++ Firefox/user.js
@@ -0,0 +1,4 @@
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnCloseOtherTabs", false);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("general.warnOnAboutConfig", false);
